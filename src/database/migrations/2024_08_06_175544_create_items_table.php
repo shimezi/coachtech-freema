037668f50c20961b19e5980f60250634ee4918fe
shimeji <satellite0902@gmail.com>
2024-08-08 14:40:21 +0900

    テーブル作成

---
+++ src/database/migrations/2024_08_06_175544_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->text('description');
+            $table->string('img_url');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_item_id')->constrained('category_items')->onDelete('cascade');
+            $table->foreignId('condition_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
